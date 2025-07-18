b9434068c0208c76763e3536dd96d25d806011a1
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2025-07-18 16:58:07 +0900

    feat : 커플 연결이 되면 채팅방 상태 업데이트

--- src/main/java/makeus/cmc/malmo/application/service/CoupleService.java
+++ src/main/java/makeus/cmc/malmo/application/service/CoupleService.java
@@ -4,12 +4,16 @@ import lombok.RequiredArgsConstructor;
 import makeus.cmc.malmo.application.port.in.CoupleLinkUseCase;
 import makeus.cmc.malmo.application.port.out.SaveCouplePort;
 import makeus.cmc.malmo.application.port.out.SendSseEventPort;
+import makeus.cmc.malmo.domain.model.chat.ChatRoom;
 import makeus.cmc.malmo.domain.model.couple.Couple;
 import makeus.cmc.malmo.domain.model.member.Member;
+import makeus.cmc.malmo.domain.service.ChatRoomDomainService;
+import makeus.cmc.malmo.domain.value.id.ChatRoomId;
 import makeus.cmc.malmo.domain.value.id.InviteCodeValue;
 import makeus.cmc.malmo.domain.value.id.MemberId;
 import makeus.cmc.malmo.domain.service.CoupleDomainService;
 import makeus.cmc.malmo.domain.service.InviteCodeDomainService;
+import makeus.cmc.malmo.domain.value.state.ChatRoomState;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +26,8 @@ public class CoupleService implements CoupleLinkUseCase {
 
     private final InviteCodeDomainService inviteCodeDomainService;
     private final CoupleDomainService coupleDomainService;
+    private final ChatRoomDomainService chatRoomDomainService;
+
     private final SendSseEventPort sendSseEventPort;
 
     private final SaveCouplePort saveCouplePort;
@@ -42,6 +48,12 @@ public class CoupleService implements CoupleLinkUseCase {
         );
 
         Couple savedCouple = saveCouplePort.saveCouple(couple);
+
+        // 커플 연결 전 PAUSED 상태의 채팅방을 ALIVE 상태로 변경
+        ChatRoom chatRoom = chatRoomDomainService.getCurrentChatRoomByMemberId(MemberId.of(command.getUserId()));
+        if (chatRoom.getChatRoomState() == ChatRoomState.ALIVE) {
+            chatRoomDomainService.updateChatRoomStateToAlive(ChatRoomId.of(chatRoom.getId()));
+        }
 
         sendSseEventPort.sendToMember(
                 MemberId.of(partner.getId()),
