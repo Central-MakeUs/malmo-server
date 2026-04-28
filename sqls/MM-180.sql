4555207051b8d6753d02411e34f0ccb6f32092f2
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2026-04-28 17:43:45 +0900

    docs: 마이그레이션용 통합 SQL 추가

--- sqls/MM-180.sql
+++
@@ -1,7 +0,0 @@
--- MM-180: Add direct partner love type field to member_entity
--- Author: Codex
--- Date: 2026-03-16
-
-ALTER TABLE member_entity
-ADD COLUMN partner_love_type_category VARCHAR(255);
-COMMENT ON COLUMN member_entity.partner_love_type_category IS 'Partner love type category: STABLE_TYPE, ANXIETY_TYPE, AVOIDANCE_TYPE, CONFUSION_TYPE, UNKNOWN';
